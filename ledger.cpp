ee60c1fd3fb4d82fda0ae0ca277381a716022bb7
Howard Hinnant <howard.hinnant@gmail.com>
2020-01-17 15:34:45 -0500

    Modify to find ledger by close_time

--- ledger.cpp
+++ ledger.cpp
@@ -1,82 +1,71 @@
-#include <string.h>
-#include <unistd.h>
-#include <fstream>
-#include <sstream>
+#include "../date/include/date/date.h"
+#include <cmath>
+#include <memory>
+#include <string>
+#include <tuple>
 #include <curl/curl.h>
 #include <json/json.h>
 
-// This is very quick and dirty code to retrieve a ledger
-// by sequence number from the S2 full history cluster
+// CURL tools
 
-char *out_buf;
-
-bool pretty = true;
-std::string space = " ";
-std::string eol = "\n";
-std::string indent = "\t";
-
-// Switch from pretty-printing to compact JSON
-void setCompact()
+static
+int
+curl_global()
 {
-    pretty = false;
-    space = "";
-    eol = "";
-    indent = "";
+    if (::curl_global_init(CURL_GLOBAL_DEFAULT) != 0)
+        throw std::runtime_error("CURL global initialization failed");
+    return 0;
 }
 
-// Structure to pass to the CURL write callback
-struct user_data
+namespace
 {
-    char *buf;
-    int so_far;
+
+struct curl_deleter
+{
+    void operator()(CURL* p) const
+    {
+        ::curl_easy_cleanup(p);
+    }
 };
 
-// The CURL code needs this callback
-size_t write_callback (char *ptr, size_t size, size_t len, void *userdata)
+}  // unnamed namespace
+
+static
+std::unique_ptr<CURL, curl_deleter>
+curl_init()
 {
-    struct user_data* ud = (struct user_data*) userdata;
-    memcpy(ud->buf + ud->so_far, ptr, size * len);
-    ud->so_far += size * len;
-    return size * len;
+    static const auto curl_is_now_initiailized = curl_global();
+    (void)curl_is_now_initiailized;
+    return std::unique_ptr<CURL, curl_deleter>{::curl_easy_init()};
 }
 
-// Write a JSON object to a stream
-void writeJson(std::ofstream& f, int step, Json::Value const &v)
+static
+bool
+post_and_download_to_string(const std::string& url, std::string const& post,
+                            std::string& reply)
 {
-    std::string pre;
-    std::string str;
-
-    if (! pretty)
+    reply.clear();
+    auto curl = curl_init();
+    if (!curl)
+        return false;
+    std::string version;
+    curl_easy_setopt(curl.get(), CURLOPT_USERAGENT, "curl");
+    curl_easy_setopt(curl.get(), CURLOPT_URL, url.c_str());
+    curl_write_callback write_cb = [](char* contents, std::size_t size, std::size_t nmemb,
+                                      void* userp) -> std::size_t
     {
-        Json::FastWriter w;
-        str = w.write (v);
-    }
-    else
-    {
-        for (int i = 0; i < step; ++i)
-            pre += indent;
-        Json::StyledStreamWriter w (indent);
-        std::ostringstream out;
-        w.write (out, v);
-        std::istringstream in(out.str());
-        str = out.str();
-    }
-
-    // Read the output line-by-line, apply
-    // formatting as needed, and write to output
-    std::string l;
-    bool first = true;
-    std::istringstream in (str);
-    while (std::getline (in, l))
-    {
-        if ((l == indent) || (l == ""))
-            continue;
-        if (first)
-            first = false;
-        else
-            f << eol;
-        f << pre << l;
-    }
+        auto& userstr = *static_cast<std::string*>(userp);
+        auto realsize = size * nmemb;
+        userstr.append(contents, realsize);
+        return realsize;
+    };
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEFUNCTION, write_cb);
+    curl_easy_setopt (curl.get(), CURLOPT_POSTFIELDSIZE, post.size());
+    curl_easy_setopt (curl.get(), CURLOPT_POSTFIELDS, post.c_str());
+    curl_easy_setopt(curl.get(), CURLOPT_WRITEDATA, &reply);
+    curl_easy_setopt(curl.get(), CURLOPT_SSL_VERIFYPEER, false);
+    auto res = curl_easy_perform(curl.get());
+    return (res == CURLE_OK);
 }
 
 // Execute a query against the S2 cluster of full history XRP Ledger notes.
@@ -96,33 +85,13 @@ bool do_query (std::string const& method, Json::Value const& params, Json::Value
         q = w.write (query);
     }
 
-    CURL* c = curl_easy_init();
-    if (c == NULL)
+    std::string out;
+    if (!post_and_download_to_string("http://s2.ripple.com:51234", q, out))
         return false;
-
-    struct user_data ud;
-    ud.buf = out_buf;
-    ud.so_far = 0;
-
-    CURLcode code;
-    code = curl_easy_setopt (c, CURLOPT_URL, "http://s2.ripple.com:51234");
-    code = curl_easy_setopt (c, CURLOPT_POSTFIELDSIZE, q.size());
-    code = curl_easy_setopt (c, CURLOPT_POSTFIELDS, q.c_str());
-    code = curl_easy_setopt (c, CURLOPT_WRITEFUNCTION, write_callback);
-    code = curl_easy_setopt (c, CURLOPT_WRITEDATA, &ud);
-
-    code = curl_easy_perform (c);
-    if (code != CURLE_OK)
-    {
-        curl_easy_cleanup (c);
-        return false;
-    }
-    ud.buf[ud.so_far] = 0;
-    curl_easy_cleanup (c);
 
     Json::Reader reader;
     Json::Value root;
-    if (!reader.parse(out_buf, root))
+    if (!reader.parse(out.c_str(), root))
     {
         fprintf(stderr, "%s\n", reader.getFormatedErrorMessages().c_str());
         return false;
@@ -150,7 +119,10 @@ bool getHeader (unsigned ledger_seq, Json::Value& header)
 {
     Json::Value reply;
     Json::Value params = Json::objectValue;
-    params["ledger_index"] = ledger_seq;
+    if (ledger_seq == 0)
+        params["ledger_index"] = "validated";
+    else
+        params["ledger_index"] = ledger_seq;
     if (! do_query ("ledger", params, reply))
     {
         header = reply;
@@ -163,170 +135,98 @@ bool getHeader (unsigned ledger_seq, Json::Value& header)
     return false;
 }
 
-// Get the transactions from a ledger given its sequence number
-// (with metadata)
-bool getTxns (unsigned ledger_seq, Json::Value& txns)
+std::pair<int, int>
+get_last_validated_close_time()
 {
     Json::Value reply;
-    Json::Value params = Json::objectValue;
-    params["ledger_index"] = ledger_seq;
-    params["transactions"] = true;
-    params["expand"] = true;
-    if (! do_query ("ledger", params, reply))
-        return false;
-    txns = std::move (reply["result"]["ledger"]);
-    if (txns.isNull() || !txns.isObject())
-        return false;
-    txns = std::move (txns["transactions"]);
-    return txns.isArray() && !txns.isNull();
+    if (getHeader(0, reply))
+        return {reply["ledger_index"].asInt(), reply["close_time"].asInt()};
+    return {0, 0};
 }
 
-// Get a chunk of a ledger's state tree
-bool getState (unsigned ledger_seq, Json::Value& entries, Json::Value& marker)
+int
+get_close_time(unsigned ledger_seq)
 {
     Json::Value reply;
-    Json::Value params = Json::objectValue;
-    params["ledger_index"] = ledger_seq;
-    params["binary"] = false;
-    if (! marker.isNull())
-        params["marker"] = marker;
-    if (! do_query ("ledger_data", params, reply))
-        return false;
-    reply = std::move (reply["result"]);
-    marker = reply["marker"];
-    entries = std::move(reply["state"]);
-    return true;
-}
-
-// Old school hacky progress indicator
-// Sorry, not sorry
-void do_progress(int max, Json::Value const& marker)
-{
-    static int so_far = 0;
-    static int phase = 0;
-    char phases[4] = { '\\', '|', '/', '-' };
-
-    int progress = max;
-    if (marker.isString())
-    {
-        // Hashes are randomly distributed, so we can tell how much
-        // progress we made by where we are in the hash space.
-        // This code only looks at the first two characters, 00-FF.
-        std::string m = marker.asString();
-        progress = 0;
-        if (m[0] >= '0' && m[0] <= '9') progress += 16 * (m[0] - '0');
-        if (m[0] >= 'A' && m[0] <= 'Z') progress += 16 * (m[0] + 10 - 'A');
-        if (m[1] >= '0' && m[1] <= '9') progress += m[1] - '0';
-        if (m[1] >= 'A' && m[1] <= 'Z') progress += m[1] + 10 - 'A';
-        progress *= max;
-        progress /= 255;
-    }
-    std::cout << "\x8 \x8";
-    while (so_far < progress)
-    {
-        std::cout << "*";
-        ++so_far;
-    }
-    std::cout << phases[++phase % 4] << std::flush;
-}
-
-int main(int argc, char* argv[])
-{
-    if (argc != 2)
-    {
-        fprintf (stderr, "You must specify a ledger by index\n");
-        return -1;
-    }
-
-    // When compiling, define "COMPACT" if you want your JSON
-    // to be as short as possible instead of being neatly
-    // spaced and indented. 
-#ifdef COMPACT
-    setCompact();
-#endif
-
-    out_buf = (char *) malloc (100000000);
-    if (out_buf == NULL)
-        return -1;
-
-    unsigned seq = (unsigned) atoll(argv[1]);
-    printf("Seq = %u\n", seq);
-
-    char fbuf[512];
-    sprintf(fbuf, "ledger.%u", seq);
-    std::ofstream f (fbuf, std::ofstream::out | std::ofstream::trunc);
-    if (! f.is_open())
-        return -1;
-
-    f << "{" << eol;
-
-    Json::Value reply;
-    if (! getHeader(seq, reply))
-    {
-        fprintf(stderr, "Query failed");
-        std::cerr << reply;
-        return -1;
-    }
-    f << indent << "\"ledger\"" << space << ":" << eol;;
-    writeJson (f, 1, reply);
-    f << "," << eol;
-
-    printf("Got header\n");
-
-    // If transaction_hash is all zeroes, there are no transactions to get
-    if (reply["transaction_hash"].asString() !=
-        "0000000000000000000000000000000000000000000000000000000000000000")
-    {
-        if (! getTxns(seq, reply))
-        {
-            fprintf(stderr, "Query failed\n");
-            return -1;
-        }
-        f << indent << "\"transactions\":" << eol;
-        writeJson (f, 1, reply);
-        f << indent << "," << eol;;
-        printf("Got %d transaction(s)\n", reply.size());
-    }
-    else printf("No transactions\n");
-    printf("Getting state tree:\n");
-    int e = 0;
-
-    std::string const progress = "0%--10%--20%--30%--40%--50%--60%--70%--80%--90%--100%";
-    std::cout << progress << std::endl;
-
-    bool first = true;
-    f << indent << "\"state\"" << space << ":" << eol << indent << "[" << eol;
-    Json::Value marker = Json::nullValue;
-    do
-    {
-        if (! getState (seq, reply, marker))
-        {
-            fprintf(stderr, "Query failed\n");
-            return -1;
-        }
-        for (unsigned j = 0u; j < reply.size(); ++j)
-        {
-            // We need to write out each state entry
-            // separately so we don't get start of object
-            // and end of object stuff from the chunk
-            if (first)
-                first = false;
-            else
-                f << "," << eol;
-            writeJson (f, 2, reply[j]);
-            ++e;
-        }
-        do_progress (progress.size(), marker);
-        // empty returned marker indicates last chunk of data
-    } while (!  marker.isNull());
-
-    std::cout << "\x8 " << std::endl;
-    printf("%d state entries\n", e);
-
-    f << eol << indent << "]" << eol;
-    f << "}" << eol;
-    f.close();
-    printf("File \"%s\" written\n", fbuf);
-
+    if (getHeader(ledger_seq, reply))
+        return reply["close_time"].asInt();
     return 0;
+}
+
+int
+main()
+{
+    using namespace std::chrono;
+    using namespace date;
+    constexpr sys_seconds epoch = sys_days{2000_y/1/1};
+
+    auto target = sys_days{2014_y/November/15} + 9h + 53min + 10s  -  epoch;
+    std::cout << "Looking for {ledger at, " << target/1s << ", " << target+epoch << "}\n";
+
+    int l1, t1;
+    std::tie(l1, t1) = get_last_validated_close_time();
+    auto l2 = l1 - 10;
+    auto t2 = get_close_time(l2);
+
+    while (true)
+    {
+        auto m = double(l2-l1)/(t2-t1);
+        auto b = l1 - m*t1;
+        auto nl = static_cast<int>(std::round(m*(target/1s) + b));
+
+        if (l1 > l2)
+        {
+            std::swap(l1, l2);
+            std::swap(t1, t2);
+        }
+
+        int* pnt;  // pointer to new guess' timestamp
+        if (nl < l1)
+        {   // If the guess is extrapolated below, chase it with our worst previous guess
+            l2 = nl;
+            pnt = &t2;
+        }
+        else if (nl > l2)
+        {   // If the guess is extrapolated above, chase it with our worst previous guess
+            l1 = nl;
+            pnt = &t1;
+        }
+        else if (nl == l1)
+        {   // If the guess is the lower bound and the upper bound is one away
+            if (l2 - l1 == 1)
+                break;  // The answer is the lower bound
+            // Else set the upper bound to one above the lower bound and try again
+            l2 = nl = l1 + 1;
+            pnt = &t2;
+        }
+        else if (nl == l2)
+        {   // If the guess is the upper bound and the lower bound is one away
+            if (l1 == l2-1)
+            {
+                // The answer is the upper bound
+                l1 = l2;
+                t1 = t2;
+                break;
+            }
+            // Else set the lower bound to one below the upper bound and try again
+            l1 = nl = l2 - 1;
+            pnt = &t1;
+        }
+        else  // Else the guess is interpolated between the lower and upper bounds
+        {   // Move the worst guess to the new guess and try again
+            if (nl - l1 <= l2 - nl)
+            {
+                l2 = nl;
+                pnt = &t2;
+            }
+            else
+            {
+                l1 = nl;
+                pnt = &t1;
+            }
+        }
+        *pnt = get_close_time(nl);
+        std::cout << '{' << nl << ", " << *pnt << ", " << seconds{*pnt}+epoch << "}\n";
+    }
+    std::cout << '{' << l1 << ", " << t1 << ", " << seconds{t1}+epoch << "}\n";
 }
